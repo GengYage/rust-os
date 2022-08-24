5c930f35a47a6eeeb62f259bb82de887e5b72644
GengYage <icoderdev@outlook.com>
2022-08-24 22:46:21 +0800

    vga mode and println macro

--- src/main.rs
+++ src/main.rs
@@ -6,12 +6,13 @@ mod vga_buffer;
 use core::panic::PanicInfo;
 
 #[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("info:{}", info);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
-    vga_buffer::print_something();
+    println!("hello world");
     loop {}
 }
--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -1,3 +1,8 @@
+use core::fmt;
+use volatile::Volatile;
+use lazy_static::lazy_static;
+use spin::Mutex;
+
 #[repr(u8)]
 #[allow(dead_code)]
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
@@ -38,17 +43,17 @@ struct ScreenChar {
 }
 
 const BUFFER_WIDTH: usize = 80;
-#[allow(dead_code)]
 const BUFFER_HEIGHT: usize = 25;
+const VGA_BUFFER_ADDR: usize = 0xb8000;
 
 // 一屏
 #[repr(transparent)]
 struct Buffer {
-    chars: [[ScreenChar; BUFFER_WIDTH]; BUFFER_WIDTH],
+    chars: [[Volatile<ScreenChar>; BUFFER_WIDTH]; BUFFER_WIDTH],
 }
 
 pub struct Writer {
-    column_position: usize,
+    char_numbers: usize,
     color_code: ColorCode,
     buffer: &'static mut Buffer,
 }
@@ -58,29 +63,49 @@ impl Writer {
         match byte {
             b'\n' => self.new_line(),
             byte => {
-                if self.column_position >= BUFFER_WIDTH {
-                    self.new_line();
-                }
-
                 // 计算当前位置
-                let col = self.column_position;
-                let row = col / BUFFER_WIDTH;
+                let mut row = self.char_numbers / BUFFER_WIDTH;
+                let col = self.char_numbers % BUFFER_WIDTH;
+
+                // 行数溢出,置为最后一行
+                if row >= (BUFFER_HEIGHT - 1) {
+                    row = BUFFER_HEIGHT - 1;
+                }
 
                 // 写入字符串
                 let color_code = self.color_code;
-                self.buffer.chars[row][col] = ScreenChar {
+                self.buffer.chars[row][col].write(ScreenChar {
                     ascii_character: byte,
                     color_code,
-                };
+                });
 
                 // 位置+1
-                self.column_position += 1;
+                self.char_numbers += 1;
             }
         }
     }
 
     fn new_line(&mut self) {
-        todo!()
+        // 若到达最大高度,所有字符串前移一行
+        if self.char_numbers / BUFFER_WIDTH >= BUFFER_HEIGHT {
+            for row in 1..BUFFER_HEIGHT {
+                for col in 0..BUFFER_WIDTH {
+                    let character = self.buffer.chars[row][col].read();
+                    //上移一行
+                    self.buffer.chars[row - 1][col].write(character);
+                }
+            }
+        }
+        // 清空最后一行
+        self.clear_row(BUFFER_HEIGHT - 1);
+
+        // 先防止整数溢出
+        if self.char_numbers >= (usize::MAX - BUFFER_WIDTH * BUFFER_HEIGHT) {
+            // 与一页字符数取模
+            self.char_numbers = self.char_numbers % (BUFFER_WIDTH * BUFFER_HEIGHT);
+        }
+
+        self.char_numbers = (self.char_numbers / BUFFER_WIDTH + 1) * BUFFER_WIDTH;
     }
 
     pub fn write_string(&mut self, s: &str) {
@@ -94,16 +119,46 @@ impl Writer {
             }
         }
     }
+
+    fn clear_row(&mut self, row: usize) {
+        let blank = ScreenChar {
+            ascii_character: b' ',
+            color_code: self.color_code,
+        };
+        for col in 0..BUFFER_WIDTH {
+            self.buffer.chars[row][col].write(blank);
+        }
+    }
 }
 
-pub fn print_something() {
-    let mut writer = Writer {
-        column_position: 0,
-        color_code: ColorCode::new(Color::Green, Color::Black),
-        buffer: unsafe { &mut *(0xb8000 as *mut Buffer) },
-    };
+impl fmt::Write for Writer {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        self.write_string(s);
+        Ok(())
+    }
+}
 
-    writer.write_byte(b'H');
-    writer.write_string("ello ");
-    writer.write_string("Wörld!");
+lazy_static! {
+    pub static ref WRITER: Mutex<Writer> = Mutex::new(Writer {
+        char_numbers: 0,
+        color_code: ColorCode::new(Color::Green, Color::Black),
+        buffer: unsafe { &mut *(VGA_BUFFER_ADDR as *mut Buffer) },
+    });
+}
+
+#[macro_export]
+macro_rules! println {
+    () => {print!("\n")};
+    ($($arg:tt)*) => (print!("{}\n", format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::vga_buffer::_print(format_args!($($arg)*)));
+}
+
+#[doc(hidden)]
+pub fn _print(args: fmt::Arguments) {
+    use core::fmt::Write;
+    WRITER.lock().write_fmt(args).unwrap();
 }
