8d981720bde997227839551365fdd261e7e45bad
Yage <gengyage@xylink.com>
2022-08-25 19:53:56 +0800

    add test framework

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,39 @@
 #![no_std]
 #![no_main]
 
+#![feature(custom_test_frameworks)]
+#![test_runner(crate::test::test_runner)]
+#![reexport_test_harness_main = "test_main"]
+
 mod vga_buffer;
+mod serial;
+mod test;
 
 use core::panic::PanicInfo;
+use crate::test::{exit_qemu, QemuExitCode};
 
+#[cfg(not(test))]
 #[panic_handler]
 fn panic(info: &PanicInfo) -> ! {
-    println!("info:{}", info);
+    println!("{}", info);
+    loop {}
+}
+
+#[cfg(test)]
+#[panic_handler]
+fn panic(info: &PanicInfo) -> ! {
+    serial_println!("[failed]\n");
+    serial_println!("Error: {}\n", info);
+    exit_qemu(QemuExitCode::Failed);
     loop {}
 }
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     println!("hello world");
+
+    #[cfg(test)]
+    test_main();
+
     loop {}
 }
